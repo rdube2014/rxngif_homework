37aa77cca0c0936d430171b8a8b86506232a0ea7
Richard J Dube <rdube2014@kellogg.northwestern.edu>
2013-11-04 22:45:10 -0600

    Part 1 v2

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,4 +2,9 @@ class PicturesController < ApplicationController
   def show
   	@picture = Picture.find(params[:id])
   end
+
+
+  def index
+  	@list_of_pictures = Picture.all  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rxngif::Application.routes.draw do
   get("/picture_details/:id", { :controller => "pictures", :action => "show" })
+
+  get("/all_pictures", {:controller => "pictures", :action => "index"})
 end
