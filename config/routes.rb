5d7289a54685cc405a27da6f0f15d458111128ec
Richard J Dube <rdube2014@kellogg.northwestern.edu>
2013-11-05 12:54:45 -0600

    Part 3 & 4

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rxngif::Application.routes.draw do
   get("/new_picture_form", { :controller => "pictures", :action => "new" })
 
   get("/create_picture", { :controller => "pictures", :action => "create" })
+
+  get("/delete_picture/:id", { :controller => "pictures", :action => "destroy" })
+
+  get("/edit_picture_form/:id", { :controller => "pictures", :action => "edit" })
+
 end
