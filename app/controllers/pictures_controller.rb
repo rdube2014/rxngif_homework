18a488ba8f730cde2ef084687b60a59608b5187c
Richard J Dube <rdube2014@kellogg.northwestern.edu>
2013-11-05 11:44:00 -0600

    Part 2 v2

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,4 +7,9 @@ class PicturesController < ApplicationController
   def index
   	@list_of_pictures = Picture.all  
   end
+
+  def create
+  	#@picture_info = params[:new_picture_form])
+  end
+
 end
