4a637d673a1729b7a0d39140f01d580269ffa6a4
Richard J Dube <rdube2014@kellogg.northwestern.edu>
2013-11-13 18:01:47 -0600

    Line ending weirdness

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,28 +2,45 @@ class PicturesController < ApplicationController
  
   def show
   	@picture = Picture.find(params[:id])
-  end
+    end
 
 
   def index
-  	@list_of_pictures = Picture.all  
-  end
+  	@list_of_pictures = Picture.order("caption")  
+   end
+
+def new
+
+end
 
   def create
-  	@picture = Picture.new
-    @picture.caption = params[:caption]
-    @picture.source = params[:source]
-    @picture.save
+  	p = Picture.new
+    p.caption = params[:caption]
+    p.source = params[:source]
+    p.save
+   
+    redirect_to pictures_url,  :notice => "Picture created successfully." 
   end
+
+def destroy
+  p = Picture.find(params[:id])
+  p.destroy
+  redirect_to pictures_url, :notice => "Picture deleted successfully."
+end 
 
   def edit
   	@picture = Picture.find(params[:id])
   end
 
    def update
-  	@picture = Picture.find(params[:id])
-    @picture.caption = params[:caption]
-    @picture.source = params[:source]
-    @picture.save
+  	p = Picture.find(params[:id])
+    p.caption = params[:caption]
+    p.source = params[:source]
+    p.save
+
+    redirect_to picture_url(p.id), :notice => "Updated picture successfully."
   end
 end
+
+
+#Why aren't the notices appearing???
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,35 @@
 Rxngif::Application.routes.draw do
-  get("/picture_details/:id", { :controller => "pictures", :action => "show" })
+  
+  # Routes for the Photo resource:
+  # CREATE
+  get '/photos/new', controller: 'photos', action: 'new', as: 'new_photo'
+  post '/photos', controller: 'photos', action: 'create', as: 'photos'
 
-  get("/all_pictures", {:controller => "pictures", :action => "index"})
+  # READ
+  get '/photos', controller: 'photos', action: 'index'
+  get '/photos/:id', controller: 'photos', action: 'show', as: 'photo'
 
-  get("/new_picture_form", { :controller => "pictures", :action => "new" })
+  # UPDATE
+  get '/photos/:id/edit', controller: 'photos', action: 'edit', as: 'edit_photo'
+  patch '/photos/:id', controller: 'photos', action: 'update'
 
-  get("/create_picture", { :controller => "pictures", :action => "create" })
+  # DELETE
+  delete '/photos/:id', controller: 'photos', action: 'destroy'
+  #------------------------------
 
-  get("/delete_picture/:id", { :controller => "pictures", :action => "destroy" })
+# CREATE
+  get "/pictures/new",  :controller => "pictures", :action => "new", :as => "new_picture" 
+  post "/pictures",  :controller => "pictures", :action => "create", :as => "pictures"
 
-  get("/edit_picture_form/:id", { :controller => "pictures", :action => "edit" })
+# READ
+  get "/pictures", :controller => "pictures", :action => "index"
+  get "/pictures/:id",  :controller => "pictures", :action => "show", :as => "picture" 
+  
+# UPDATE
+  get "/pictures/:id/edit",  :controller => "pictures", :action => "edit", :as => "edit_picture" 
+  patch "/pictures/:id",  :controller => "pictures", :action => "update"
 
-  get("/update_picture/:id", { :controller => "pictures", :action => "update" })
-
+# DELETE
+  delete "/pictures/:id",  :controller => "pictures", :action => "destroy"
 end
 
